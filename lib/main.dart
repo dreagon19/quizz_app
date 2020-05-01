1658da8a2af51c973599e48ee657e5a48acbc211
dreagon19 <anjit193023@gmail.com>
2020-05-01 16:06:24 +0530

    adding files

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'quiz_Brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrian quizBrian = QuizBrian();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userAns)
+  {
+      bool correctAns = quizBrian.getqans();
+
+      setState(() {
+        if(quizBrian.isFinished() == true)
+          {
+            Alert(
+              context: context,
+              type: AlertType.error,
+              title: "FINISHED",
+              desc: "Hurray!! you have completed the Quiz",
+              buttons: [
+                DialogButton(
+                  child: Text(
+                    "DONE",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                  width: 120,
+                )
+              ],
+            ).show();
+            quizBrian.reset();
+            scoreKeeper = [];
+          }
+      });
+
+
+
+      if (userAns== correctAns){
+        // right ans
+        scoreKeeper.add(Icon(Icons.check,color: Colors.green));
+      }
+      else{
+        //wrong ans
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+      }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrian.getqText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                // picked true
+
+                checkAnswer(true);
+               setState(() {
+                  quizBrian.nextq();
+
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                checkAnswer(false);
+               setState(() {
+                  quizBrian.nextq();
+                  });},
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
